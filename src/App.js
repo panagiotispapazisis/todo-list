f701a6a3d671601d1ef2fde49d5704c47a35c945
panagiotispapazisis <panagiotis60@outlook.com>
2020-11-25 14:47:41 +0200

    create Form and TodoList comps + imr to the app.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import React from "react";
 import "./App.css";
+import Form from "./components/Form";
+import TodoList from "./components/TodoList";
 function App() {
-  return <div>Hello React</div>;
+  return (
+    <div className="App">
+      <header>
+        <h1>Todo List</h1>
+      </header>
+      <Form />
+      <TodoList />
+    </div>
+  );
 }
 
 export default App;
