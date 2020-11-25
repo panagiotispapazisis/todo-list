7002ec6f67fcfb12829e4a63d03800f7aa579399
panagiotispapazisis <panagiotis60@outlook.com>
2020-11-25 14:51:58 +0200

    write the code for form , todolist to render them

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,7 +1,23 @@
 import React from "react";
 
 function Form() {
-  return <div></div>;
+  return (
+    <div>
+      <form>
+        <input type="text" class="todo-input" />
+        <button class="todo-button" type="submit">
+          <i class="fas fa-plus-square"></i>
+        </button>
+        <div class="select">
+          <select name="todos" class="filter-todo">
+            <option value="all">All</option>
+            <option value="completed">Completed</option>
+            <option value="uncompleted">Uncompleted</option>
+          </select>
+        </div>
+      </form>
+    </div>
+  );
 }
 
 export default Form;
