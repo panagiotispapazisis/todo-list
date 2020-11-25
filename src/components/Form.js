48ff7d233677dbecb91f905cfbc0950263cc307f
panagiotispapazisis <panagiotis60@outlook.com>
2020-11-25 15:22:18 +0200

    console log the event values of input

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,10 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Form() {
+  const inputTextHandler = (e) => {
+    console.log(e);
+    console.log(e.target);
+    console.log(e.target.value);
+  };
   return (
     <div>
       <form>
-        <input type="text" class="todo-input" />
+        <input onChange={inputTextHandler} type="text" class="todo-input" />
         <button class="todo-button" type="submit">
           <i class="fas fa-plus-square"></i>
         </button>
