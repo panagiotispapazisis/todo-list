b15fbc2fd33f1e90f582463caa1782704161a5f9
panagiotispapazisis <panagiotis60@outlook.com>
2020-11-25 15:01:07 +0200

    write the code for todo component to render

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,7 +1,17 @@
 import React from "react";
 
 function Todo() {
-  return <div></div>;
+  return (
+    <div className="todo">
+      <li className="todo-item">Hey</li>
+      <button className="complete-btn">
+        <i className="fas fa-check"></i>
+      </button>
+      <button className="trash-btn">
+        <i className="fas fa-trash"></i>
+      </button>
+    </div>
+  );
 }
 
 export default Todo;
